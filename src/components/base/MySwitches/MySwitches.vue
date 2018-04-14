79f200c5381e5e0a651ea7677cb64bb0827226c6
kuangpengfei <1633397286@qq.com>
2018-04-14 22:36:38 +0800

    components MySwitches

---
+++ src/components/base/MySwitches/MySwitches.vue
@@ -0,0 +1,59 @@
+<template>
+  <ul class="my-switches">
+    <li class="switch-item" :class="{'active':currentIndex === index}" v-for="(item, index) in switches" @click="switchItem(index)">
+      <span>{{ item.name }} </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  props: {
+    switches: {
+      type: Array,
+      default: []
+    },
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    // 开关被切换了，通知父组件
+    switchItem(index) {
+      this.$emit('switch', index)
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/common/scss/const.scss";
+@import "~@/common/scss/mixin.scss";
+
+.my-switches {
+  display: flex;
+  align-items: center;
+  width: 240px;
+  margin: 0 auto;
+  border: 1px solid $color-highlight-background;
+  border-radius: 5px;
+  .switch-item {
+    flex: 1;
+    padding: 8px;
+    text-align: center;
+    font-size: $font-size-medium;
+    color: $color-text-d;
+    &.active {
+      background: $color-highlight-background;
+      color: $color-text;
+    }
+  }
+}
+</style>
