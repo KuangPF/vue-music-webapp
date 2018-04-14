fb591d0160ac195415b5c35ae05dfee7521a3da3
kuangpengfei <1633397286@qq.com>
2018-04-15 00:00:44 +0800

    add my-scroll

--- src/components/MyRecommend/MyRecommend.vue
+++ src/components/MyRecommend/MyRecommend.vue
@@ -1,10 +1,22 @@
+<!-- 推荐页组件 -->
+
 <template>
-  <div></div>
+  <div class="my-recommmend">
+    <!-- better-scroll 滚动组件，当请求到 lists 时才渲染 -->
+    <my-scroll>
+      <div>
+        <!-- 轮播图，当请求到 recommends 时才渲染 -->
+      </div>
+    </my-scroll>
+  </div>
 </template>
 
 <script>
+import MyScroll from '@/components/base/MyScroll/MyScroll';
 export default {
-
+  components: {
+    MyScroll
+  }
 };
 </script>
 
