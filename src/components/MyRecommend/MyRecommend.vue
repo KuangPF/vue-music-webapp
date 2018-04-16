a19fc61947beb4ee8fda0046e4fc7b397b47d154
kuangpengfei <1633397286@qq.com>
2018-04-16 22:55:50 +0800

    my-slider #2

--- src/components/MyRecommend/MyRecommend.vue
+++ src/components/MyRecommend/MyRecommend.vue
@@ -6,6 +6,15 @@
     <my-scroll>
       <div>
         <!-- 轮播图，当请求到 recommends 时才渲染 -->
+        <div v-if="recommends.length" class="slide-wrapper">
+          <my-slider>
+            <div v-for="(recommend,index) in recommends" :key="index">
+              <a :href="recommend.linkUrl">
+                <img @load="loadImg" :src="recommend.picUrl" class="needsclick">
+              </a>
+            </div>
+          </my-slider>
+        </div>
       </div>
     </my-scroll>
   </div>
@@ -13,9 +22,16 @@
 
 <script>
 import MyScroll from '@/components/base/MyScroll/MyScroll';
+import MySlider from '@/components/base/MySlider/MySlider';
 export default {
+  data() {
+    return {
+      recommends: []
+    };
+  },
   components: {
-    MyScroll
+    MyScroll,
+    MySlider
   }
 };
 </script>
--- src/components/base/MySlider/MySlider.vue
+++ src/components/base/MySlider/MySlider.vue
@@ -5,21 +5,21 @@
     </div>
     <!-- 轮播点 -->
     <div class="dots">
-      <span v-for="(dot, index) in dots" :class="{ active: currentDotsIndex === index }" class="dot"></span>
+      <span v-for="(dot, index) in dots" :key="index" :class="{ active: currentDotsIndex === index }" class="dot"></span>
     </div>
   </div>
 </template>
 
 <script>
 import BScroll from 'better-scroll';
-import { myDom } from '@/common/js/myutils.js';
+import { myDOM } from '@/common/js/myutils.js';
 export default {
   components: {},
   data() {
     return {
       dots: [],
       currentDotsIndex: 0
-    }
+    };
   },
   props: {
     // 是否循环播放
@@ -42,27 +42,26 @@ export default {
     // 轮播图(sliderGroup)宽度
     _setSliderWidth(isResize) {
       // 拿到传过来的图片
-      this.children = this.$refs.sliderGroup.children
+      this.children = this.$refs.sliderGroup.children;
       // console.log(this.children) // (5) [div, div, div, div, div]
 
       // 拿到父元素（slider）宽度
-      let width = 0
-      let sliderWidth = this.$refs.slider.clientWidth
+      let width = 0;
+      let sliderWidth = this.$refs.slider.clientWidth;
 
       // 动态添加 class、width
       for (let i = 0; i < this.children.length; i++) {
-        let child = this.children[i]
-        myDOM.addClass(child, 'slider-item')
+        let child = this.children[i];
+        myDOM.addClass(child, 'slider-item');
 
-        child.style.width = sliderWidth + 'px'
-        width += sliderWidth
+        child.style.width = sliderWidth + 'px';
+        width += sliderWidth;
       }
 
       if (this.loop && !isResize) {
-        width += 2 * sliderWidth
+        width += 2 * sliderWidth;
       }
-
-      this.$refs.sliderGroup.style.width = width + 'px'
+      this.$refs.sliderGroup.style.width = width + 'px';
     },
     // 初始化轮播图
     _initSlider() {
@@ -74,69 +73,69 @@ export default {
         snapLoop: this.loop,
         snapThreshold: 0.3,
         snapSpeed: 400
-      })
+      });
 
       this.slider.on('scrollEnd', () => {
-        let nowIndex = this.slider.getCurrentPage().pageX
+        let nowIndex = this.slider.getCurrentPage().pageX;
         // 循环模式下 -1
         if (this.loop) {
-          nowIndex -= 1
+          nowIndex -= 1;
         }
-        this.currentDotsIndex = nowIndex
+        this.currentDotsIndex = nowIndex;
 
         // 重置自动轮播定时器
         if (this.autoPlay) {
-          clearTimeout(this.timer)
-          this._initPlay()
+          clearTimeout(this.timer);
+          this._initPlay();
         }
-      })
+      });
     },
     // 初始化轮播点
     _initDots() {
-      this.dots = new Array(this.children.length)
+      this.dots = new Array(this.children.length);
     },
     // 自动播放
     _initPlay() {
-      let nextIndex = this.currentDotsIndex + 1
+      let nextIndex = this.currentDotsIndex + 1;
       if (this.loop) {
-        nextIndex += 1
+        nextIndex += 1;
       }
       this.timer = setTimeout(() => {
-        this.slider.goToPage(nextIndex, 0, 400)
-      }, this.delay)
+        this.slider.goToPage(nextIndex, 0, 400);
+      }, this.delay);
     }
   },
   created() { },
   mounted() {
     setTimeout(() => {
-      this._setSliderWidth()
-      this._initDots()
-      this._initSlider()
+      this._setSliderWidth();
+      this._initDots();
+      this._initSlider();
 
       if (this.autoPlay) {
-        this._initPlay()
+        this._initPlay();
       }
 
       // 当窗口尺寸改变时，重新计算轮播宽度
       window.addEventListener('resize', () => {
         if (!this.slider) {
-          return
+          return;
         }
-        this._setSliderWidth(true)
-        this.slider.refresh()
-      })
-    }, 20)
+        this._setSliderWidth(true);
+        this.slider.refresh();
+      });
+    }, 20);
   },
   destroyed() {
     // 良好的习惯：销毁定时器
-    clearTimeout(this.timer)
+    clearTimeout(this.timer);
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
 @import "~@/common/scss/const.scss";
-@import "~@/common/scss/mymixin.scss";
+@import "~@/common/scss/mixin.scss";
 
 .my-slider {
   min-height: 1px;
