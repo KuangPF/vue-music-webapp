3823db82486bd535bc996971101c75c9de57c4be
KuangPF <1633397286@qq.com>
2018-04-18 15:47:21 +0800

    add list

--- src/components/MySinger/MySinger.vue
+++ src/components/MySinger/MySinger.vue
@@ -27,11 +27,11 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         console.log(res);
-        this.singers = this._formatSinger(res.data.list);
+        this.singers = this._formatSingers(res.data.list);
       });
     },
     // 重组 res.data.list 数据
-    _formatSinger(list) {
+    _formatSingers(list) {
       // 热门歌手
       let map = {
         hot: {
@@ -41,12 +41,42 @@ export default {
       };
 
       // 填充歌手数据
-      list.forEach((index, item) => {
+      list.forEach((item, index) => {
         // 填充热门歌手数据
         if (index < HOT_NUM) {
           map.hot.items.push(createSinger(item));
         };
+
+        // 填充 a-z 字母
+        let key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        // 填充对应字母歌手数据
+        map[key].items.push(createSinger(item));
       });
+
+      // 得到有序列表
+      let hot = [];
+      let others = [];
+
+      for (let key in map) {
+        let value = map[key];
+        if (value.title.match(/[a-zA-Z]/)) {
+          others.push(value);
+        } else if (value.title === HOT_TITLE) {
+          hot.push(value);
+        }
+      }
+
+      others.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      console.log(hot.concat(others));
+      return hot.concat(others);
     }
   }
 };
