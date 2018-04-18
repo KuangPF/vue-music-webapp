26220372e347fe0251826d36d09e4d1197dcb2d5
KuangPF <1633397286@qq.com>
2018-04-18 15:05:59 +0800

    my-singer #3

--- src/components/MySinger/MySinger.vue
+++ src/components/MySinger/MySinger.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="my-singer">
-    <my-phone-list></my-phone-list>
+    <my-phone-list :data="singers"></my-phone-list>
   </div>
 </template>
 
 <script>
-import MyPhoneList from '@/components/base/MyPhoneList/MyPhoneList';
 import { getSingerList } from '@/api/singer.js';
+import { createSinger } from '@/common/js/SingerClass.js';
+import MyPhoneList from '@/components/base/MyPhoneList/MyPhoneList';
+
+const HOT_TITLE = '热门';
+const HOT_NUM = 10;
 export default {
   components: {
     MyPhoneList
   },
   data() {
     return {
-
+      singers: []
     };
   },
   created() {
@@ -23,6 +27,25 @@ export default {
     _getSingerList() {
       getSingerList().then((res) => {
         console.log(res);
+        this.singers = this._formatSinger(res.data.list);
+      });
+    },
+    // 重组 res.data.list 数据
+    _formatSinger(list) {
+      // 热门歌手
+      let map = {
+        hot: {
+          title: HOT_TITLE,
+          items: []
+        }
+      };
+
+      // 填充歌手数据
+      list.forEach((index, item) => {
+        // 填充热门歌手数据
+        if (index < HOT_NUM) {
+          map.hot.items.push(createSinger(item));
+        };
       });
     }
   }
