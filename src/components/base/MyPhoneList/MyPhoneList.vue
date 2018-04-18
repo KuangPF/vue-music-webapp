6fb70235e242a252f4da067bb49174feced7aada
KuangPF <1633397286@qq.com>
2018-04-18 16:11:40 +0800

    update

--- src/components/base/MyPhoneList/MyPhoneList.vue
+++ src/components/base/MyPhoneList/MyPhoneList.vue
@@ -3,6 +3,12 @@
     <ul>
       <li ref="leftRef" v-for="(group,index) in data" class="list-group" :key="index">
         <h2 class="list-group-title">{{ group.title }}</h2>
+        <ul>
+          <li v-for="(item,index) in group.items" class="list-group-item" :key="index">
+            <img v-lazy="item.avatar" class="avatar">
+            <span class="name">{{ item.name }}</span>
+          </li>
+        </ul>
       </li>
     </ul>
   </my-scroll>
