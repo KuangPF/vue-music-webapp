fcf4e669f587401aedaaef65f6cf25731ae22ff0
kuangpengfei <1633397286@qq.com>
2018-04-29 23:57:42 +0800

    禁止刷新歌单详情页（获取不到歌单 id）

--- src/components/MySongListDetail/MySongListDetail.vue
+++ src/components/MySongListDetail/MySongListDetail.vue
@@ -1,14 +1,33 @@
 <template>
   <transition name="slide">
-    <my-music-list ></my-music-list>
+    <my-music-list :title="title"></my-music-list>
   </transition>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import MyMusicList from '@/components/MyMusicList/MyMusicList';
 export default {
   components: {
     MyMusicList
+  },
+  computed: {
+    ...mapGetters(['songlist']),
+    title() {
+      return this.songlist.dissname;
+    }
+  },
+  methods: {
+    _getSongList() {
+      // 禁止直接刷新详情页(获取不到歌单 id)
+      if (!this.songlist.dissid) {
+        this.$router.push('/recommend');
+        return; // eslint-disable-line
+      }
+    }
+  },
+  created() {
+    this._getSongList();
   }
 };
 </script>
