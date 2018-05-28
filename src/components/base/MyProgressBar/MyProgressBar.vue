04f12ef818bd0f546e114b701608364e11694100
kuangpengfei <1633397286@qq.com>
2018-05-28 23:11:32 +0800

    clickProgress init

--- src/components/base/MyProgressBar/MyProgressBar.vue
+++ src/components/base/MyProgressBar/MyProgressBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="progress-bar" ref="progressBar">
+  <div class="progress-bar" ref="progressBar" @click="clickProgress">
     <div class="bar-inner">
       <div class="progress" ref="progress"></div>
       <div class="progress-btn-wrapper" @touchstart.prevent="progressTouchStart" @touchmove.prevent="progressTouchMove" @touchend="progressTouchEnd" ref="progressBtn">
