f1bbf5c0f43b26f3d7b7b28917e9a6cefa7f2354
kuangpengfei <1633397286@qq.com>
2018-06-22 23:25:38 +0800

    feature:add hot key

--- src/components/MySearch/MySearch.vue
+++ src/components/MySearch/MySearch.vue
@@ -3,14 +3,25 @@
     <div class="search-box-wrapper">
       <my-search-box></my-search-box>
     </div>
+    <div ref="shortcutWrapper" class="shortcut-wrapper">
+      <my-scroll ref="shortcut" class="shortcut">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+          </div>
+        </div>
+      </my-scroll>
+    </div>
   </div>
 </template>
 
 <script>
 import MySearchBox from '@/components/base/MySearchBox/MySearchBox';
+import MyScroll from '@/components/base/MyScroll/MyScroll';
 export default {
   components: {
-    MySearchBox
+    MySearchBox,
+    MyScroll
   }
 };
 </script>
